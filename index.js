e103d3d5c6a5e689b64d428d7e7ac7704aecd2ef
swhachey <sw.hachey@gmail.com>
2021-02-18 15:04:03 -0700

    created my first inquirer prompt for manager

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const promptMain = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'mgmtname',
+      message: 'What is your Team Managers name?',
+    },
+    {
+      type: 'input',
+      name: 'mgmtid',
+      message: 'What is the managers ID#',
+    },
+    {
+      type: 'input',
+      name: 'mgmtemail',
+      message: 'What is the managers email address?',
+    },
+    {
+      type: 'input',
+      name: 'mgmtnum',
+      message: 'What is the managers office phone #?',
+    },
+    {
+      type: 'list',
+      name: 'github',
+      message: 'Add employee or finish team?',
+      choices: ["Engineer","Intern","Finish Team"]
+    },
+  ])
+  .then((data) => {
+      console.log(`made it`)
+  })
+};
+
+promptMain();
